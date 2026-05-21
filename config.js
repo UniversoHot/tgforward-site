e051de4c890d0d2c75da713e479c67b9bade2305
UniversoHot <martinsbarachorennan@gmail.com>
2026-05-21 00:32:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,1 @@
-// Opcional: sobrescreva a API base quando hospedar o frontend separado.
-// Exemplo:
-// window.TG_FORWARD_API_BASE = "https://tgforward-backend.onrender.com/api";
-window.TG_FORWARD_API_BASE = window.TG_FORWARD_API_BASE || "";
+window.TG_FORWARD_API_BASE = "https://tgforward-backend.onrender.com/api";
